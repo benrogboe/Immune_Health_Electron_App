587864d309a9ef49df37dbbfa8b0d7123a0b73a4
Eric Gauzens <ericgauzens@hotmail.com>
2022-06-23 10:42:28 -0400

    refactored store to accomodate file upload page

--- src/views/StudiesBrowser.vue
+++ src/views/StudiesBrowser.vue
@@ -40,9 +40,7 @@
 import IhSubheader from '@/components/shared/IhSubheader.vue'
 import IhButton from '@/components/shared/IhButton.vue'
 import BfNavigationSecondary from '@/components/bf-navigation/BfNavigationSecondary.vue'
-import { mapActions, mapGetters } from 'vuex'
-import { isEmpty, propOr } from 'ramda'
-import axios from 'axios'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'StudiesBrowser',
@@ -51,34 +49,8 @@ export default {
     IhButton,
     BfNavigationSecondary
   },
-  data() {
-    return {
-      allStudies: []
-    }
-  },
-  async mounted() {
-    // HARDCODED FOR NOW: UPDATE apiKey VALUE WITH A VALID LOGGED IN USER API TOKEN TO GET STUDIES POPULATED
-    const apiKey = 'eyJraWQiOiJwcjhTaWE2dm9FZTcxNyttOWRiYXRlc3lJZkx6K3lIdDE4RGR5aGVodHZNPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5NzY0NGZkYy1iZjFmLTRkMzUtOWRlMC1hYzlkODUxZmEyZTEiLCJkZXZpY2Vfa2V5IjoidXMtZWFzdC0xXzZkNDc4M2U3LThhNTctNGY0Yy1iODQ5LWYzZGExMGUzMmRlNSIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX2IxTnl4WWNyMCIsImNsaWVudF9pZCI6IjY3MG1vN3NpODFwY2Mzc2Z1YjdvMTkxNGQ4Iiwib3JpZ2luX2p0aSI6ImFhYjEwZTA5LTk3ZjEtNDBjMy05MGFiLTU1ZmI1ZWY3NWM5YyIsImV2ZW50X2lkIjoiM2Q4ZDYwMTktZThlOS00MzQ4LWE3MmQtZjlkNWYzNzg2ZWZmIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJhd3MuY29nbml0by5zaWduaW4udXNlci5hZG1pbiIsImF1dGhfdGltZSI6MTY1NTkwNjAzMCwiZXhwIjoxNjU1OTk0MjUwLCJpYXQiOjE2NTU5OTA2NTAsImp0aSI6IjIwOTFiNTA5LTFlYmQtNDk5Ni1hODVlLWNlYzUxODk4MTRkMSIsInVzZXJuYW1lIjoiOTc2NDRmZGMtYmYxZi00ZDM1LTlkZTAtYWM5ZDg1MWZhMmUxIn0.EaYQAWBh6T1GsTkmB4TmK6YvpE5wSDcDmvuUSS0MNHkP3wO7knOCAczIts2elOGRpjT8IzIzyiHzmmQ2ILw5RQtwq_Y-Htq5Wg6LOZ5lmG1-j-zLCpw1QDNaIIDmSNf2NvO5HU2KtHIFUad86r_oeGGZMSu9S87A0SnyKmwjR9LSUN-w8AoBIs5X9dT2vHS9SxyeZK2GAtONKurspE7d1CzgVOa737aCIevLHaJVFWX8-dY8Vhd0LBXCFlEUiJNE8yXIF1A1hRdU2kHp1K1qRw0IV9ohRnSWHRKrUlDr-d5pGmOVgzC7xxrGbN5-NWI81WEAK0yGW7r1bn4q3zNIdw'
-    const studiesUrl = `https://api.pennsieve.io/models/datasets/N:dataset:e2de8e35-7780-40ec-86ef-058adf164bbc/concepts/33a61ee7-fce9-4f0c-823c-78368ed8dc42/instances?api_key=${apiKey}`
-    
-    await axios.get(studiesUrl).then(response => this.allStudies = response.data)
-
-    if (!isEmpty(this.allStudies.length) && isEmpty(this.selectedStudy)) {
-      this.setSelectedStudy(this.allStudies[0])
-    }
-  },
-  methods: {
-    ...mapActions(['setSelectedStudy']),
-  },
   computed: {
-    ...mapGetters(['selectedStudy']),
-    selectedStudyName() {
-      const studyValues = propOr([], 'values', this.selectedStudy)
-      if (isEmpty(studyValues)) {
-        return ''
-      }
-      return propOr('', 'value', studyValues[0])
-    }
+    ...mapGetters(['allStudies', 'selectedStudy', 'selectedStudyName']),
   }
 }
 </script>
